db73782d4788300685787e7efb8de08e9744f17f
Ethan Graham <ethangraham43@gmail.com>
2022-10-31 17:05:55 -0400

    passed a couple specs

--- skeleton/bin/p02_controller_server.rb
+++ skeleton/bin/p02_controller_server.rb
@@ -1,8 +1,39 @@
 require 'rack'
 require_relative '../lib/controller_base'
+require "byebug"
 
 class MyController < ControllerBase
+  def initialize(req, res)
+    @req = req
+    @res = res
+
+  end
+
+  def render_content(content, content_type = 'text/html')
+    @already_built_response ||= true
+    # debugger
+    if @already_built_response
+      @already_built_response = false
+      @res.write(content)
+      @res['Content-Type'] = content_type
+      debugger
+    end
+  end
+
+  def redirect_to(url)
+    debugger
+    @already_built_response ||= true
+    if @already_built_response
+      debugger
+      @already_built_response = false
+      @res.status = 302
+      @res['Location'] = url
+      nil
+    end
+  end
+
   def go
+    # debugger
     if req.path == "/cats"
       render_content("hello cats!", "text/html")
     else
